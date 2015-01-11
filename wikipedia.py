c9cd6edf42883bbbc6f4079c59de168bb3395f0b
David Batley <unplug@dbatley.com>
2015-01-11 01:04:30 +0000

    Merge wikipedia files together

--- wikipedia.py
+++ wikipedia.py
@@ -1,15 +1,87 @@
-import wikipedia_api
-import wikitext_parser
 import logging
 import json
+import urllib
+import json
+import re
+from shared import Candidate
+
+def latest_revision(pagename):
+	logging.debug("latest_revision(%r)", pagename)
+	assert isinstance(pagename, unicode)
+	req = urllib.urlopen("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=%s&rvprop=content&format=json" % (urllib.quote(pagename.encode("utf8")),))
+	page = req.read()
+	try:
+		data = json.loads(page)
+		page_data, = data["query"]["pages"].values()
+		revision, = page_data["revisions"]
+		return revision["*"]
+	except Exception:
+		logging.exception("Invalid response for %r", page)
+		raise
+
+def parliament_constituencies():
+	page = latest_revision("List of United Kingdom Parliament constituencies")
+	table = re.search(r'class="wikitable sortable"(.*?)\|}\s*==', page, re.DOTALL).group(1)
+	after_tr = True
+	for line in table.split("\n")[1:]:
+		if line.startswith("!"):
+			continue
+		if line.startswith("|-"):
+			after_tr = True
+			continue
+		if after_tr:
+			after_tr = False
+			m = re.search(r"^\s*\|\s*\[\[([^\]\|]+)(?:\|[^\]]*)?\]\]", line)
+			if m is None:
+				raise ValueError(line)
+			yield m.group(1).strip()
+
+def section_for_2015(page):
+	for section in re.findall(r"\{\{Election box begin.*?\{\{Election box end\}\}", page, re.DOTALL | re.I):
+		if "United Kingdom general election, 2015" in section or "Next United Kingdom general election" in section or "|General Election 2015]]" in section:
+			assert section.lower().count("election box begin") == 1
+			return section
+	# does not have any 2015 section
+	return None
+
+def candidates_from_section(section):
+	candidates = re.findall(r"\{\{Election box candidate.*?\n\s*\}\}", section, re.DOTALL | re.I)
+	candidates_check = re.findall(r"candidate\s*=", section, re.DOTALL | re.I)
+	if len(candidates_check) != len(candidates):
+		raise ValueError((candidates, candidates_check))
+	return [parse_candidate_wikitext(c) for c in candidates]
+
+def remove_wikilink(name):
+	m = re.search("^\[\[(.*)\]\]$", name.strip())
+	if m is not None:
+		return m.group(1).split("|", 1)[-1]
+	return name
+
+def remove_references(name):
+	return re.sub(r"<ref.*?</ref>", "", name)
+
+def parse_candidate_wikitext(wikitext):
+	try:
+		name = re.search(r"^[\s\|]*candidate\s*=(.*)$", wikitext, re.M).group(1).strip()
+	except Exception:
+		raise ValueError(wikitext) # name
+	name = remove_wikilink(remove_references(name))
+	name = name.strip()
+	try:
+		party = re.search(r"^[\s\|]*party\s*=(.*)$", wikitext, re.M).group(1).strip()
+	except Exception:
+		raise ValueError(wikitext) # party
+	party = remove_wikilink(party)
+	party = party.strip()
+	return Candidate(name, party)
 
 def fetch_and_parse_candidates(constituency_name):
 	try:
-		page = wikipedia_api.latest_revision(constituency_name).encode("utf8")
-		section = wikitext_parser.section_for_2015(page)
+		page = latest_revision(constituency_name).encode("utf8")
+		section = section_for_2015(page)
 		if section is None:
 			raise Exception("No section on page")
-		return list(wikitext_parser.candidates_from_section(section))
+		return list(candidates_from_section(section))
 	except Exception:
 		logging.exception("Unable to parse %r" % (constituency_name,))
 		return []
@@ -18,7 +90,7 @@ if __name__=='__main__':
 	logging.root.setLevel(logging.WARN)
 
 	data = {}
-	for constituency_name in wikipedia_api.parliament_constituencies():
+	for constituency_name in parliament_constituencies():
 		candidates = fetch_and_parse_candidates(constituency_name)
 		logging.debug("Found %d candidates for %r", len(candidates), constituency_name)
 		data[constituency_name] = candidates
