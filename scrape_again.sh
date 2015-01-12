66bfa4d9727563a5399359f65c9c5e89a4a3d5fb
David Batley <hello@dbatley.com>
2015-01-12 00:48:24 +0000

    remove mappings.csv file: these are entirely auto-generated

--- scrape_again.sh
+++ scrape_again.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 python wikipedia.py || echo "FAILED TO UPDATE WIKIPEDIA"
 python ynmp.py || echo "FAILED TO UPDATE YNMP"
+python merge.py || echo "FAILED MERGE"
+python merge_candidates.py || echo "FAILED MERGE"
 python generate_output_html.py || echo "FAILED TO GENERATE output.html"
 
