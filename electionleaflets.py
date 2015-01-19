820ab39da69daef37190571547172dfc6ef8e9a8
David Batley <hello@dbatley.com>
2015-01-19 00:16:13 +0000

    Use last 50 leaflets, print to html

--- electionleaflets.py
+++ electionleaflets.py
@@ -4,9 +4,11 @@ import re
 import logging
 import json
 import sys
+import time
 from combined_data import normalise_names
 from mapping import MappingDatabase
 from party_names import lookup_long_name as party_name_lookup
+from shared import escape_html as escape
 
 def getpage(url):
 	req = urllib.urlopen(url)
@@ -16,6 +18,11 @@ def rss_links():
 	page = getpage("http://www.electionleaflets.org/feeds/latest/")
 	doc = etree.XML(page)
 	return tuple(str(x) for x in doc.xpath("channel/item/link/text()"))
+
+def most_recent_id():
+	link = tuple(rss_links())[0]
+	id = int(re.search(r"/leaflets/(\d+)/", link).group(1))
+	return id
 
 def fetch_leaflet(url):
 	page = getpage(url)
@@ -31,6 +38,10 @@ def match_el_ynmp(constituency_map, el_data, ynmp):
 	except KeyError:
 		logging.debug("ynmp_constituency mapping missing: not mapped %r (from %r)", canonical_name, ynmp.keys())
 		raise
+	if el_party == "Other":
+		# we can't make any assumptions about it
+		logging.info("Leaflet for Other %r", el_url)
+		return None
 	party = party_name_lookup[el_party] # can KeyError on no mapping
 	ynmp_party_name = party.ynmp
 	matching_candidates = [ can for can in ynmp_constituency if can["party"] == ynmp_party_name ]
@@ -46,20 +57,28 @@ def match_el_ynmp(constituency_map, el_data, ynmp):
 if __name__=='__main__':
 	logging.root.setLevel(logging.WARN)
 
+	leaflets_to_fetch = 50
+
 	with open("mappings_constituency.csv", "r") as f:
 		constituency_map = MappingDatabase.load(f)
 	with open("candidates_from_ynmp.json", "r") as f:
 		ynmp = json.load(f)
-
 	ynmp = normalise_names(constituency_map, "ynmp", ynmp)
-	for url in rss_links():
+
+	print "<h1>Unknown candidates from last %d election leaflets</h1>" % (leaflets_to_fetch,)
+
+	new_last_fetched_id = most_recent_id()
+	for id in xrange(new_last_fetched_id - leaflets_to_fetch, new_last_fetched_id+1):
+		url = "http://www.electionleaflets.org/leaflets/%d/" % (id,)
 		try:
 			el_data = fetch_leaflet(url)
 			new_candidate = match_el_ynmp(constituency_map, el_data, ynmp)
 			if new_candidate is not None:
-				print new_candidate
+				el_constituency_name, _, el_party = new_candidate
+				print "<a href=\"%s\">%s %s</a> (ynmp <br/>" % (url, escape(el_constituency_name), escape(el_party),)
 		except Exception:
 			logging.info("Cannot parse leaflet page %r", url, exc_info=sys.exc_info())
+	print "(generated %r)" % (time.ctime(),)
 
 	with open("mappings_constituency.csv", "w") as f:
 		constituency_map.save(f)
