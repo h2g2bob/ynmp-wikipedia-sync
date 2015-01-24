202ed9cc68bf3ce9e72c52f5fd5b7b56d17b42db
David Batley <hello@dbatley.com>
2015-01-24 01:17:34 +0000

    party names

--- party_names.py
+++ party_names.py
@@ -4,19 +4,21 @@ Party = namedtuple("Party", "ynmp wikipedia short alternatives")
 parties = (
 	Party("Labour Party", "Labour Party (UK)", "L", ["The Labour Party"]),
 	Party("Conservative Party", "Conservative Party (UK)", "C", []),
-	Party("Liberal Democrats", "Liberal Democrats", "LD", []),
-	Party("Green Party", "Green Party of England and Wales" , "G", []),
+	Party("Liberal Democrats", "Liberal Democrats", "LD", ["Liberal Democrats (UK)"]),
+	Party("Green Party", "Green Party of England and Wales" , "G", ["Green Party (UK)"]),
 	Party("UK Independence Party (UKIP)", "UK Independence Party", "UKIP", ["United Kingdom Independence Party"]),
 	Party("Plaid Cymru - The Party of Wales", "Plaid Cymru", "PC", []),
 	Party("Scottish National Party (SNP)", "Scottish National Party", "SNP", []),
 	Party("Independent", "Independent (politician)", "Ind", []),
 	Party("Trade Unionist and Socialist Coalition", "Trade Unionist and Socialist Coalition", "TUSC", []),
-	Party("Alliance - Alliance Party of Northern Ireland", "Alliance", "A", []),
+	Party("Alliance - Alliance Party of Northern Ireland", "Alliance", "A", ["Alliance Party of Northern Ireland"]),
 	Party("SDLP (Social Democratic & Labour Party)", "Social Democratic and Labour Party", "SDLP", []),
-	Party(u'Sinn F\xe9in', u'Sinn F\xe9in', "SF", []),
-	Party("Class War", None, "CW", []),
+	Party(u'Sinn F\xe9in', u'Sinn F\xe9in', "SF", ["Sinn Fein"]),
+	Party("Class War", "Class War", "CW", []),
 	Party("National Health Action Party", "National Health Action Party", "NHA", []),
 	Party("Democratic Unionist Party - D.U.P.", "Democratic Unionist Party", "DUP", []),
+	Party("Mebyon Kernow - The Party for Cornwall", "Mebyon Kernow", "MK", []),
+	Party("Ulster Unionist Party", "Ulster Unionist Party", "UUP", []),
 )
 
 lookup_long_name = {}
