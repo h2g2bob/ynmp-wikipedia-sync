20aff332be9c262a41632cf240fcaf7ff855a392
David Batley <hello@dbatley.com>
2015-01-27 01:31:07 +0000

    use a mixture of covered and intersects for eu regions

--- mapit.py
+++ mapit.py
@@ -3,12 +3,22 @@ import json
 import logging
 import re
 
+def fetch_eu_region(mapit_area_id, context):
+	req = urllib.urlopen("http://mapit.mysociety.org/area/%d/%s?type=EUR" % (mapit_area_id, context,))
+	page = req.read()
+	return json.loads(page)
+
 def eu_region_from_url(mapit_area_url):
 	logging.debug("eu_region_from_url(%r)", mapit_area_url)
 	mapit_area_id = int(re.search(r"^http://mapit.mysociety.org/area/(\d+)$", mapit_area_url).group(1))
-	req = urllib.urlopen("http://mapit.mysociety.org/area/%d/intersects?type=EUR" % (mapit_area_id,))
-	page = req.read()
-	covered_regions_dict = json.loads(page)
-	data, = covered_regions_dict.values()
-	return data["name"]
+	covered_regions_dict = fetch_eu_region(mapit_area_id, "covered")
+	if len(covered_regions_dict) == 0:
+		logging.info("Using intersects for eu_region_from_url(%r)", mapit_area_url)
+		covered_regions_dict = fetch_eu_region(mapit_area_id, "intersects")
+	if len(covered_regions_dict) == 1:
+		data, = covered_regions_dict.values()
+		return data["name"]
+	else:
+		logging.error("eu_region_from_url(%r) -> %r", mapit_area_url, covered_regions_dict)
+		return "unknown"
 
